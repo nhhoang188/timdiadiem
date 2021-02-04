410f8e51e4d26100c56c80de7f5a49e94ad42724
nhhoang188@gmail.com <ghnhh18896>
2021-02-04 22:03:29 +0700

    update view admin

--- src/main/java/com/timdiadiem/controller/AdminController.java
+++ src/main/java/com/timdiadiem/controller/AdminController.java
@@ -1,4 +1,35 @@
 package com.timdiadiem.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
+    @GetMapping
+    public ModelAndView admin() {
+        return null;
+    }
+
+    @GetMapping("edit-hotel")
+    public ModelAndView editHotel() {
+        return null;
+    }
+
+    @GetMapping("edit-tour")
+    public ModelAndView editTour() {
+        return null;
+    }
+
+    @GetMapping("edit-user")
+    public ModelAndView editUser() {
+        return null;
+    }
+
+    @GetMapping("edit-blog")
+    public ModelAndView editBlog() {
+        return null;
+    }
 }
