aefd035459f17a74a6259d579d95844027082fed
nhhoang188@gmail.com <ghnhh18896>
2021-02-10 23:03:19 +0700

    model room

--- src/main/java/com/timdiadiem/model/Room.java
+++ src/main/java/com/timdiadiem/model/Room.java
@@ -1,12 +1,77 @@
 package com.timdiadiem.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 @Entity
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private Integer status;
+    private String type;
+    private String description;
+    private Double price;
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    public Room(Long id, Integer status, String type, String description, Double price, Hotel hotel) {
+        this.id = id;
+        this.status = status;
+        this.type = type;
+        this.description = description;
+        this.price = price;
+        this.hotel = hotel;
+    }
+
+    public Room() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
 }
