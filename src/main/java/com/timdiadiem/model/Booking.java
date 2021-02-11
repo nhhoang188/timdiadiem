7a5775b6dcb8304b13451f816f9d3c4a859e1f46
nhhoang188@gmail.com <ghnhh18896>
2021-02-11 10:22:39 +0700

    model booking

--- src/main/java/com/timdiadiem/model/Booking.java
+++ src/main/java/com/timdiadiem/model/Booking.java
@@ -8,12 +8,19 @@ public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(name = "startdate")
+    private Date startDate;
+    @Column(name = "enddate")
+    private Date  endDate;
+    @Column(name = "totalprice")
+    private Double totalPrice;
 //    @ManyToOne
 //    @JoinColumn(name = "user_id")
 //    private User user;
-//    @ManyToOne
-//    @JoinColumn( name = "room_id")
-//    private Room room;
-    private Date startDate, endDate;
-    private Double totalPrice;
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public Booking() {
+    }
 }
