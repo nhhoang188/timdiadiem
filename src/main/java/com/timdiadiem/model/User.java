014d501f2e1f016795bb4e0b8eefb840772d7a3b
nhhoang188@gmail.com <ghnhh18896>
2021-02-11 10:38:07 +0700

    fix model user

--- src/main/java/com/timdiadiem/model/User.java
+++ src/main/java/com/timdiadiem/model/User.java
@@ -16,8 +16,8 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String firstName;
-    private String lastName;
+    private String firstname;
+    private String lastname;
     private String username;
     private String password;
     @Email
@@ -25,17 +25,17 @@ public class User implements UserDetails {
     private String phone;
     private String address;
     @ManyToOne
-    @JoinColumn(name = "bank_id")
+    @JoinColumn( name = "bank_id")
     private BankAcount bankAcount;
     @Enumerated(EnumType.STRING)
-    private UserRole userRole;
+    private UserRole userrole;
     private boolean enabled;
     private boolean locked;
 
     // override
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority authority  = new SimpleGrantedAuthority(userRole.name());
+        SimpleGrantedAuthority authority  = new SimpleGrantedAuthority(userrole.name());
         return Collections.singletonList(authority);
     }
 
@@ -71,114 +71,27 @@ public class User implements UserDetails {
 
     // constructors
 
-    public User(String firstName
-            , String lastName
+    public User(String firstname
+            , String lastname
             , String username
             , String password
             , @Email String email
             , String phone
             , String address
             , BankAcount bankAcount
-            , UserRole userRole
+            , UserRole userrole
             , boolean enabled
             , boolean locked) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstname = firstname;
+        this.lastname = lastname;
         this.username = username;
         this.password = password;
         this.email = email;
         this.phone = phone;
         this.address = address;
         this.bankAcount = bankAcount;
-        this.userRole = userRole;
+        this.userrole = userrole;
         this.enabled = enabled;
-        this.locked = locked;
-    }
-
-    public User() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public BankAcount getBankAcount() {
-        return bankAcount;
-    }
-
-    public void setBankAcount(BankAcount bankAcount) {
-        this.bankAcount = bankAcount;
-    }
-
-    public UserRole getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public boolean isLocked() {
-        return locked;
-    }
-
-    public void setLocked(boolean locked) {
         this.locked = locked;
     }
 }
