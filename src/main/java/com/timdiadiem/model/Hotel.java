355eeb79b05a677614196fcd3c4b799f1ead6853
nhhoang188@gmail.com <ghnhh18896>
2021-02-24 23:00:13 +0700

    hotel complete

--- src/main/java/com/timdiadiem/model/Hotel.java
+++ src/main/java/com/timdiadiem/model/Hotel.java
@@ -15,20 +15,19 @@ public class Hotel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name")
     private String name;
-    @Column(name = "address")
     private String address;
     @Column(name = "sortdescription")
     private String sortDescription;
     @Column(name = "fulldescription")
     private String fullDescription;
-    @Column(name = "convenient")
     private String convenient;
-    private List<Room> roomList;
     private String img1;
     private String img2;
     private String img3;
     private String img4;
     private String thumbnail;
+    @OneToMany(targetEntity = Room.class)
+    private List<Room> roomlist;
+
 }
